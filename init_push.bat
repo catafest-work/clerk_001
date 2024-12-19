d1a5001940f67c6dd4d8b0e37f3f1c03111ae71a
Catalin George Festila <catafets76@gmail.com>
2024-12-19 16:55:05 +0200

    Init first commit message

--- init_push.bat
+++ init_push.bat
@@ -12,3 +12,21 @@ git commit -m "Init first commit message"
 
 # Push the changes to the remote repository
 git push origin main
+
+# Check the current branch name
+git branch
+
+# If the branch is not named 'main', rename it to 'main'
+git branch -m main
+
+# Add the remote repository URL if not already added
+git remote add origin https://github.com/catafest-work/clerk_001.git
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a message
+git commit -m "Your commit message"
+
+# Push the changes to the remote repository
+git push -u origin main
